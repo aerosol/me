abda27d6d282f61011f2647a4aea054ad584684c
Adam Rutkowski <hq@mtod.org>
2021-08-19 09:50:13 +0200

    Riiight (v2)

--- total_running_distance.sh
+++ total_running_distance.sh
@@ -1,3 +1,3 @@
 #!/bin/env sh
-cat _activities/*.json | jq -c '. | select(.activityType.typeKey ==
-"running")' | jq -n '[inputs | .distance] | add'
+echo "$(cat _activities/*.json | jq -c '. | select(.activityType.typeKey ==
+"running")' | jq -n '[inputs | .distance] | add') / 1000" | bc
