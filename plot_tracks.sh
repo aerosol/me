a3d00d23372ad1cfbcdea0ffb270e05fe759d408
Adam Rutkowski <hq@mtod.org>
2021-08-20 08:35:58 +0200

    Update stats dashboard

--- plot_tracks.sh
+++ plot_tracks.sh
@@ -3,7 +3,6 @@ mkdir -p _activities-svg
 for f in _activities/*.gpx
 do
   echo "Processing $f"
-  ./gpx2svg -i "$f" -o "$f.svg" -m 400
-  echo "$(cat $f.svg)<hr/>" >> svgs.html
+  ./gpx2svg -i "$f" -o _activities-svg/$(basename "$f.svg") -m 400
 done
 
--- stats.py
+++ stats.py
@@ -4,6 +4,10 @@ import json
 import os
 import sys
 import glob
+import string
+from datetime import date
+
+CURRENT_YEAR = date.today().year
 
 activities = []
 
@@ -16,7 +20,8 @@ def summary(activities, activity_type, field):
     return sum(a[field] for a in activities if a['activityType']['typeKey'] == activity_type)
 
 def summary_this_year(activities, activity_type, field):
-    this_year = [a for a in activities if a['startTimeGMT'].startswith('2021')]
+    this_year = [a for a in activities if a['startTimeGMT'].startswith('%d' %
+        CURRENT_YEAR)]
     return summary(this_year, activity_type, field)
 
 distance_running = summary(activities, 'running', 'distance') / 1000
@@ -37,10 +42,141 @@ duration_walking_this_year = summary_this_year(activities, 'walking', 'duration'
 duration_cycling_this_year = summary_this_year(activities, 'cycling', 'duration') / 3600
 duration_strength_this_year = summary_this_year(activities, 'indoor_cardio', 'duration') / 3600
 
-print('Distance running: %d km (%d km this year)' % (distance_running, distance_running_this_year))
-print('Distance walking: %d km (%d km this year)' % (distance_walking, distance_walking_this_year)) 
-print('Distance cycling: %d km (%d km this year)' % (distance_cycling, distance_cycling_this_year))
+def most_recent(activities, activity_type):
+    return [a for a in sorted(activities, key=lambda x: x['activityId']) if a['activityType']['typeKey'] ==
+            activity_type][-1]
 
-print('Duration running: %d h (%d h this year)' % (duration_running, duration_running_this_year))
-print('Duration walking: %d h (%d h this year)' % (duration_walking, duration_walking_this_year))
-print('Duration cycling: %d h (%d h this year)' % (duration_cycling, duration_cycling_this_year))
+def svg_gpx(activity):
+    return '<img src="_activities-svg/activity_%s.gpx.svg"/>' % activity['activityId']
+
+def svg_hr(activity):
+    ts = activity['startTimeGMT'][0:10]
+
+    with open("%s/heart.dat" % ts) as json_file:
+        data = json.load(json_file)
+        beats = []
+        ts = 10
+        for row in data['heartRateValues']:
+            ts += 1
+            pm = 200 - row[1]
+            beats.append(','.join([str(ts), str(pm)]))
+
+    svg = """
+    <svg viewBox="0 0 500 200" class="chart">
+      <polyline
+         fill="none"
+         stroke="#000000"
+         stroke-width="1"
+         points="
+         %s
+         "/>
+    </svg>
+    """ % ('\n'.join(beats))
+
+    return svg
+
+component_groups = [
+        {'name': 'Running', 'components': [
+        {'label': 'Distance (%d)' % CURRENT_YEAR, 'value': '%d km' % (distance_running_this_year )},
+        {'label': 'Duration (%d)' % CURRENT_YEAR, 'value': '%d h' % (duration_running_this_year)},
+        {'label': 'Distance (total)', 'value': '%d km' % (distance_running)},
+        {'label': 'Duration (total)', 'value': '%d h' % (duration_running)},
+        {'label': 'Last HR', 'value': svg_hr(most_recent(activities,
+            'running'))},
+        {'label': 'Last track', 'value': svg_gpx(most_recent(activities,
+            'running'))}
+
+        ]},
+
+        {'name': 'Strength', 'components': [
+        {'label': 'Duration (%d)' % CURRENT_YEAR, 'value': '%d h' % (duration_strength_this_year)},
+        {'label': 'Duration (total)', 'value': '%d h' % (duration_strength)},
+        {'label': 'Last HR', 'value': svg_hr(most_recent(activities,
+            'indoor_cardio'))},
+        ]},
+
+        {'name': 'Cycling', 'components': [
+        {'label': 'Distance (%d)' % CURRENT_YEAR, 'value': '%d km' % (distance_cycling_this_year )},
+        {'label': 'Duration (%d)' % CURRENT_YEAR, 'value': '%d h' % (duration_cycling_this_year)},
+        {'label': 'Distance (total)', 'value': '%d km' % (distance_cycling)},
+        {'label': 'Duration (total)', 'value': '%d h' % (duration_cycling)},
+        {'label': 'Last HR', 'value': svg_hr(most_recent(activities,
+            'cycling'))},
+        {'label': 'Last track', 'value': svg_gpx(most_recent(activities,
+            'cycling'))}
+        ]},
+
+        {'name': 'Walking', 'components': [
+        {'label': 'Distance (%d)' % CURRENT_YEAR, 'value': '%d km' % (distance_walking_this_year )},
+        {'label': 'Duration (%d)' % CURRENT_YEAR, 'value': '%d h' % (duration_walking_this_year)},
+        {'label': 'Distance (total)', 'value': '%d km' % (distance_walking)},
+        {'label': 'Duration (total)', 'value': '%d h' % (duration_walking)},
+        {'label': 'Last HR', 'value': svg_hr(most_recent(activities,
+            'walking'))},
+        {'label': 'Last track', 'value': svg_gpx(most_recent(activities,
+            'walking'))}
+        ]},
+]
+
+base_html = """
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="tailwind.css" rel="stylesheet">
+</head>
+<body>
+
+  ${groups}
+
+</body>
+</html>
+"""
+
+group_html = """
+  <div class="flex items-stretch">
+    <h1 class="text-4xl w-96 bg-black text-white inline p-8 font-bold">${caption}</h1>
+    <div class="max-w-7xl w-full mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="grid grid-cols-2 gap-2">
+            ${components}
+        </div>
+    </div>
+  </div>
+  <hr/>
+"""
+
+component_html = """
+            <div class="w-full">
+                <div class="widget w-full p-4 rounded-lg bg-white dark:bg-gray-900 dark:border-gray-800 align-middle">
+                    <div class="flex flex-row items-stretch">
+                        <div class="flex flex-col items-stretch">
+                            <div class="text-gray-500 align-middle">
+                                ${label}
+                            </div>
+                            <div class="text-xl font-bold align-middle">
+                                ${value}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+"""
+
+base = string.Template(base_html)
+
+
+groups = ''
+for group in component_groups:
+    components = ''
+    group_tpl = string.Template(group_html)
+
+    for data in group['components']:
+        component = string.Template(component_html)
+        components += component.substitute(data)
+
+    groups += group_tpl.substitute(components=components, caption=group['name'])
+
+render = base.substitute(groups=groups)
+
+print(render)
